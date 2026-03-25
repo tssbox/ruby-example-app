81f569e24fe5e9b0b402623559f857895cc2a924
Viachaslau <slava@tssbox.com>
2026-03-25 16:37:57 +0400

    test: add auto-generated e2e security tests
    skip-checks:true

---
+++ .brightsec/tests/patch-posts-1.test.ts
@@ -0,0 +1,46 @@
+import { test, before, after } from 'node:test';
+import { SecRunner } from '@sectester/runner';
+import { AttackParamLocation, HttpMethod } from '@sectester/scan';
+
+const timeout = 40 * 60 * 1000;
+const baseUrl = process.env.BRIGHT_TARGET_URL!;
+
+let runner!: SecRunner;
+
+before(async () => {
+  runner = new SecRunner({
+    hostname: process.env.BRIGHT_HOSTNAME!,
+    projectId: process.env.BRIGHT_PROJECT_ID!
+  });
+
+  await runner.init();
+});
+
+after(() => runner.clear());
+
+test('PATCH /posts/1', { signal: AbortSignal.timeout(timeout) }, async () => {
+  await runner
+    .createScan({
+      tests: ['csrf', 'bopla', 'sqli', 'xss', 'id_enumeration'],
+      attackParamLocations: [AttackParamLocation.BODY],
+      starMetadata: {
+        code_source: 'tssbox/ruby-example-app:master',
+        databases: ['PostgreSQL'],
+        user_roles: ['admin']
+      },
+      poolSize: +process.env.SECTESTER_SCAN_POOL_SIZE || undefined
+    })
+    .setFailFast(false)
+    .timeout(timeout)
+    .run({
+      method: HttpMethod.PATCH,
+      url: `${baseUrl}/posts/1`,
+      body: {
+        title: 'Updated Post Title',
+        content: 'Updated content of the post.',
+        public: true
+      },
+      headers: { 'Content-Type': 'application/json' },
+      auth: process.env.BRIGHT_AUTH_ID
+    });
+});
---
+++ .brightsec/tests/post-users.test.ts
@@ -0,0 +1,49 @@
+import { test, before, after } from 'node:test';
+import { SecRunner } from '@sectester/runner';
+import { AttackParamLocation, HttpMethod } from '@sectester/scan';
+
+const timeout = 40 * 60 * 1000;
+const baseUrl = process.env.BRIGHT_TARGET_URL!;
+
+let runner!: SecRunner;
+
+before(async () => {
+  runner = new SecRunner({
+    hostname: process.env.BRIGHT_HOSTNAME!,
+    projectId: process.env.BRIGHT_PROJECT_ID!
+  });
+
+  await runner.init();
+});
+
+after(() => runner.clear());
+
+test('POST /users', { signal: AbortSignal.timeout(timeout) }, async () => {
+  await runner
+    .createScan({
+      tests: ['csrf', 'bopla', 'sqli', 'xss', 'secret_tokens'],
+      attackParamLocations: [AttackParamLocation.BODY],
+      starMetadata: {
+        code_source: "tssbox/ruby-example-app:master",
+        databases: ["PostgreSQL"],
+        user_roles: ["admin"]
+      },
+      poolSize: +process.env.SECTESTER_SCAN_POOL_SIZE || undefined
+    })
+    .setFailFast(false)
+    .timeout(timeout)
+    .run({
+      method: HttpMethod.POST,
+      url: `${baseUrl}/users`,
+      body: {
+        user: {
+          email: "example@example.com",
+          password: "securepassword",
+          password_digest: "$2a$12$KIXQ1Y1rZ1u1Q1",
+          admin: false
+        }
+      },
+      headers: { 'Content-Type': 'application/json' },
+      auth: process.env.BRIGHT_AUTH_ID
+    });
+});
